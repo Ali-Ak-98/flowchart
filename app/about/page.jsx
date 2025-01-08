3faa5c9b448576c17960bfbfa97fd759c1e83e87
Ali Akbarzadeh <ali1998akbarzadeh@gmail.com>
2025-01-08 11:28:02 +0100

    fix bugs 2

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -14,7 +14,7 @@ export default async function AboutPage() {
                 <img
                     className={'w-1/3 object-contain'}
                     key={image.url}
-                    src={`${process.env.NEXT_PUBLIC_BASE_URL+image?.url}`}
+                    src={`${process.env.NEXT_PUBLIC_CONTENT_URL+image?.url}`}
                     alt={image?.alternativeText || 'About'}
                 />
             ))}
